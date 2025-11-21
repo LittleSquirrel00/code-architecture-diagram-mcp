a2393619592703e8e0928ebff002565e6b231948
zhangfeng144 <zhangfeng144@jd.com>
2025-11-22 07:37:59 +0800

    clean

--- test-resolve.ts
+++
@@ -1,9 +0,0 @@
-import { resolveImportPath } from './src/graph/builder.js'
-
-const result = resolveImportPath(
-  '/project/src/index.ts',
-  './utils'
-)
-
-console.log('Resolved:', result)
-console.log('Expected something like: /project/src/utils.ts')
